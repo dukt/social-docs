2293524aff6657205aac5508c09a9d4c1b480b3f
Benjamin David <ben@dukt.net>
2018-06-28 23:44:02 +0200

    Updating Social

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -20,6 +20,7 @@ module.exports = {
                      '',
                      'requirements',
                      'installation',
+                     'updating',
                      'configuration',
                  ]
              },
